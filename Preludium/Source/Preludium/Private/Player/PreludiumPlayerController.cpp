9e642dbb266fde42c8bf1bf5006e3a327ed05c83
Foxim <foximh2@gmail.com>
2024-12-07 19:23:32 +0100

    [Foxim] - Trying 443 problem

--- Preludium/Source/Preludium/Private/Player/PreludiumPlayerController.cpp
+++ Preludium/Source/Preludium/Private/Player/PreludiumPlayerController.cpp
@@ -2,4 +2,63 @@
 
 
 #include "Player/PreludiumPlayerController.h"
+#include "EnhancedInputComponent.h"
 
+APreludiumPlayerController::APreludiumPlayerController()
+{
+	
+}
+
+void APreludiumPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APreludiumPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	AimTrace();
+}
+
+void APreludiumPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APreludiumPlayerController::Move);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APreludiumPlayerController::Look);
+
+	
+}
+
+void APreludiumPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+void APreludiumPlayerController::Look(const FInputActionValue& InputActionValue)
+{
+	const FVector2D LookAxisVector = InputActionValue.Get<FVector2D>();
+
+	AddYawInput(LookAxisVector.X);
+	AddPitchInput(LookAxisVector.Y);
+}
+
+void APreludiumPlayerController::AimTrace()
+{
+	
+}
--- Preludium/Source/Preludium/Public/Player/PreludiumPlayerController.h
+++ Preludium/Source/Preludium/Public/Player/PreludiumPlayerController.h
@@ -6,12 +6,39 @@
 #include "GameFramework/PlayerController.h"
 #include "PreludiumPlayerController.generated.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+class UInputAction;
+
 UCLASS()
 class PRELUDIUM_API APreludiumPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	APreludiumPlayerController();
+
+	virtual void BeginPlay() override;
+	
+	virtual void PlayerTick(float DeltaTime) override;
+
+protected:
+	virtual void SetupInputComponent() override;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> MappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> LookAction;
+
+	UFUNCTION(BlueprintCallable)
+	void Move(const FInputActionValue& InputActionValue);
+
+	UFUNCTION(BlueprintCallable)
+	void Look(const FInputActionValue& InputActionValue);
+
+	void AimTrace();
 	
 };
